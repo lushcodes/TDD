f0eb9a4435f65d862d0e264a8660cd0acdce4dc3
lushcodes <lushcodes@gmail.com>
2021-07-03 14:14:45 +0100

    Unit test passed to dock a bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,9 +3,18 @@ require 'docking_station'
 describe DockingStation do
     it { is_expected.to respond_to :release_bike }
 
+    it {is_expected.to respond_to(:dock).with(1).argument }
+
+    it { is_expected.to respond_to(:bike) } 
+
     it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
-end 
+    end
+
+    it 'docks a bike' do
+        bike = Bike.new
+        expect(subject.dock(bike)).to eq bike
+    end
 end
 
